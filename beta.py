b3e361ea124e171b11cd29ce873a70d7dc2cbb7d
Sharon Zhou <sharonzhou12@gmail.com>
2017-09-25 11:11:25 -0700

    approx beta

--- beta.py
+++ beta.py
@@ -10,16 +10,51 @@ class Beta:
 		self.alpha = alpha
 		self.beta = beta
 
+	def __str__(self):
+		return "Beta(" + str(self.alpha) + ", " + str(self.beta)
+
+	def __mul__(self, other):
+		return self.compute() * other
+
+	def __add__(self, other):
+		return self.compute() + other
+
+	def __truediv__(self, other):
+		return self.compute() / float(other)
+
 	def update(self, alpha=None, beta=None):
 		if alpha:
 			self.alpha = alpha
 		if beta:
 			self.beta = beta
 		return
-
-	def function(self, function, other):
-		# TODO: allow functions to interact with this object (necessary?)
-		return function(self)
 	
 	def compute(self):
-		return np.random.beta(alpha, beta)
+		# approximation
+		n = 1000000000.
+		p = self.alpha / self.alpha + self.beta
+		return np.random.binomial(n, p) / n
+
+class Mul:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def compute(self):
+		return x * y
+
+class Add:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def compute(self):
+		return x + y
+
+class Div:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def compute(self):
+		return x / y	
--- kg.py
+++ kg.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import random
-from beta import Beta
+from beta import Beta, Mul
 
 """
 Knowledge Gradient: from forgetful Bayes and myopic planning
@@ -18,11 +18,15 @@ class KnowledgeGradient:
 		# from eval (pg 5) of paper
 		self.prior_alpha = 2
 		self.prior_beta = 2
-		self.priors = np.array([Beta(self.prior_alpha, self.prior_beta) for _ in range(n_arms)])
-		self.gamma = Beta(self.prior_alpha, self.prior_beta).compute()
-		self.thetas = np.array([prior.compute() for prior in self.priors])
-		self.q = self.prior
-		# self.states = [[s] for s in self.q]
+		self.priors = np.array([approx_beta(self.prior_alpha, self.prior_beta) for _ in range(n_arms)])
+		self.gamma = approx_beta(self.prior_alpha, self.prior_beta)
+		self.thetas = self.priors
+		self.q = None
+
+	def approx_beta(self, alpha, beta):
+		n = 1000000000.
+		p = float(alpha) / float(alpha + beta)
+		return np.random.binomial(n, p) / n
 
 	def observe_reward(self, k, reward):
 		self.t += 1
@@ -32,18 +36,19 @@ class KnowledgeGradient:
 		
 		# Update estimated reward rate for arm k
 		self.thetas[k] = self.thetas[k] + (1. / self.n_pulls[k]) * (reward - self.thetas[k])
-		# Represent as Beta dist, from which to be generated iid
-		# self.thetas[k] = Beta(self.prior_alpha + self.successes[k], self.prior_beta + self.failures[k])
 
 		# Update posteriors
-		# pr_observation = self.thetas**self.successes * (1-self.thetas)**self.failures
-		pr_observation = Beta(self.successes + 1, self.failures + 1)
-		pr_prior = gamma * self.q + (1 - gamma) * self.priors
+		pr_observation = approx_beta(self.successes + 1, self.failures + 1)
+		pr_prior = gamma * self.q + (1. - gamma) * self.priors
 		self.q = pr_observation * pr_prior
 		self.q /= np.sum(self.q)
 		return 
 
 	def choose_arm(self):
+		# TODO: expectation over hypothetical t+1 (0 or 1) for each arm, how each affects all qs (or E(\theta_k^t))
+		# take mean of random var qs w/ integral
+		# take max of these means over all arms
+		# Q: can't you just take the theta with the highest E(theta^t)? as exploit
 		n = self.successes + self.failures
 		p = np.true_divide(self.successes, n)
 		variance = n * p * (1 - p)
