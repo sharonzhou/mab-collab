7f6503e92c7040e152dad3f2e92f62b1d360529e
Sharon Zhou <sharonzhou@DN0a22a638.SUNet>
2017-09-26 13:28:59 -0700

    discretized beta pdf and included in q calculations

--- kg.py
+++ kg.py
@@ -10,32 +10,41 @@ class KnowledgeGradient:
 	def __init__(self, n_arms, T):
 		self.T = T
 		self.t = 1
+		
 		self.n_arms = n_arms
 		self.successes = np.zeros(n_arms)
 		self.failures = np.zeros(n_arms)
 
-		# from eval (pg 5) of paper
 		self.prior_alpha = 2
 		self.prior_beta = 2
-		self.priors = np.array([self._beta(self.prior_alpha, self.prior_beta) for _ in range(n_arms)])
-		self.gamma = self._beta(self.prior_alpha, self.prior_beta)
-		self.thetas = self.priors
-		self.q = self._beta(1, 1) * self.priors
+		self.priors = np.array([self._discretize_beta_pdf(self.prior_alpha, self.prior_beta) for _ in range(n_arms)])
+		
+		# NB: gamma taken empirically from paper
+		self.gamma = .81 
+		self.thetas = np.zeros(n_arms)
+		self.q = np.array([self._discretize_beta_pdf(self.prior_alpha + 1, self.prior_beta + 1) for _ in range(n_arms)])
 	
+	# Discretizes Beta dist into its pdf with support [0,1], normalized to integrate to 1
+	def _discretize_beta_pdf(self, alpha, beta):
+		x = [i / 100. for i in range(101)]
+		pdf = [i**(alpha - 1.) * (1. - i)**(beta - 1.) for i in x]
+		pdf /= np.sum(pdf)
+		return pdf
+
 	# Estimates updated reward rates + posterior probability distributions, after arm k is pulled with reward r
 	def _estimate(self, q, thetas, successes, failures, k, r, priors, gamma):
 		successes[k] += r
 		failures[k] += 1 - r
 		thetas[k] = thetas[k] + (1. / (successes[k] + failures[k])) * (r - thetas[k])
 
-		pr_observation = self._beta(successes + 1, failures + 1)
+		pr_observation = self._discretize_beta_pdf(successes + 1, failures + 1).T
 		pr_prior = gamma * q + (1. - gamma) * priors
 		q = pr_observation * pr_prior
 		q /= np.sum(q)
 
-		return thetas, np.array(q)
+		return thetas, q
 
-	def _beta(self, alpha, beta):
+	def _expected_beta(self, alpha, beta):
 		return np.true_divide(alpha, alpha + beta)
 
 	def observe_and_choose(self, k, reward):
@@ -57,7 +66,7 @@ class KnowledgeGradient:
 				self.successes, self.failures, k, 0, self.priors, self.gamma)
 
 			# Expectation of success + failure
-			expectations[k] = np.sum(q_success * thetas_success + q_failure * thetas_failure)
+			expectations[k] = np.sum((thetas_success.T * q_success.T).T + (thetas_failure.T * q_failure.T).T)
 
 		# Take max over all arms (removed independent term in gradient)
 		decisions = self.thetas + (self.T - self.t - 1) * np.amax(expectations)
