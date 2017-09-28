ae86d8d040f69cb09d6a577fe10e45b0b5f21a0a
Sharon Zhou <sharonzhou12@gmail.com>
2017-09-28 11:17:40 -0700

    calculated thetas in paper to be expectations of q

--- kg.py
+++ kg.py
@@ -21,7 +21,7 @@ class KnowledgeGradient:
 		
 		# NB: gamma taken empirically from paper
 		self.gamma = .81 
-		self.thetas = np.array([self._expected_beta(self.prior_alpha, self.prior_beta) for _ in n_arms])
+		self.thetas = np.array([np.random.beta(self.prior_alpha, self.prior_beta) for _ in range(n_arms)])
 		self.q = np.array([self._discretize_beta_pdf(self.prior_alpha + 1, self.prior_beta + 1) for _ in range(n_arms)])
 	
 	# Discretizes Beta dist into its pdf with support [0,1], normalized to integrate to 1
@@ -33,16 +33,11 @@ class KnowledgeGradient:
 
 	# Estimates updated reward rates + posterior probability distributions, after arm k is pulled with reward r
 	def _estimate(self, q, thetas, k, r, priors, gamma):
-	# def _estimate(self, q, thetas, successes, failures, k, r, priors, gamma):
-		# successes[k] += r
-		# failures[k] += 1 - r
-		# thetas[k] = thetas[k] + (1. / (successes[k] + failures[k])) * (r - thetas[k])
-
-		# Reset thetas
+		# Reset thetas randomly
 		if random.random() < 1 - gamma:
-			thetas = np.array([self._expected_beta(self.prior_alpha, self.prior_beta) for _ in self.n_arms])
+			thetas = np.array([np.random.beta(self.prior_alpha, self.prior_beta) for _ in range(self.n_arms)])
 
-		pr_observation = np.ones((n_arms, 100))
+		pr_observation = np.ones((self.n_arms, 101))
 		pr_observation[k] = [i / 100. if r else 1 - i / 100. for i in range(101)]
 		pr_prior = gamma * q + (1. - gamma) * priors
 		q = pr_observation * pr_prior
@@ -50,29 +45,16 @@ class KnowledgeGradient:
 
 		return thetas, q
 
-	def _expected_beta(self, alpha, beta):
-		return np.true_divide(alpha, alpha + beta)
-
 	def observe_and_choose(self, k, reward):
 		self.t += 1		
 
 		# Observe reward
 		self.thetas, self.q = self._estimate(self.q, self.thetas, \
 			k, reward, self.priors, self.gamma)
-		# self.thetas, self.q = self._estimate(self.q, self.thetas, \
-		# 	self.successes, self.failures, k, reward, self.priors, self.gamma)
 
 		# Choose next arm
 		expectations = np.zeros(self.n_arms)
 		for k in range(self.n_arms):
-			# Hypothetical success
-			# thetas_success, q_success = self._estimate(self.q, self.thetas, \
-			# 	self.successes, self.failures, k, 1, self.priors, self.gamma)
-			
-			# # Hypothetical failure
-			# thetas_failure, q_failure = self._estimate(self.q, self.thetas, \
-			# 	self.successes, self.failures, k, 0, self.priors, self.gamma)
-
 			# Hypothetical success
 			thetas_success, q_success = self._estimate(self.q, self.thetas, \
 				k, 1, self.priors, self.gamma)
@@ -81,12 +63,11 @@ class KnowledgeGradient:
 			thetas_failure, q_failure = self._estimate(self.q, self.thetas, \
 				k, 0, self.priors, self.gamma)
 
-
 			# Expectation of success + failure
-			expectations[k] = np.sum((thetas_success.T * q_success.T).T + (thetas_failure.T * q_failure.T).T)
+			expectations[k] = np.sum(q_success) + np.sum(q_failure)
 
 		# Take max over all arms (removed independent term in gradient)
-		decisions = self.thetas + (self.T - self.t - 1) * np.amax(expectations)
+		decisions = np.sum(self.q, axis=1) + (self.T - self.t - 1) * np.amax(expectations)
 		return np.argmax(decisions)
 
 
