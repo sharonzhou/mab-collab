fdbaccd2d2d837203759e7970dd07089a912cc7e
Sharon Zhou <sharonzhou12@gmail.com>
2017-10-09 00:45:48 -0700

    host

--- single/app/views.py
+++ single/app/views.py
@@ -45,6 +45,3 @@ def play(uid):
 @app.route('/')
 def index():
 	return render_template('index.html')
-
-if __name__ == "__main__":
-	app.run(debug=True)
--- single/run.py
+++ single/run.py
@@ -2,4 +2,5 @@ from app import app
 import os
 
 port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host="0.0.0.0", port=port)
+host = os.environ.get("HOST", "0.0.0.0")
+app.run(debug=True, host=host, port=port)
