32c0d49c66827a43f44a8131371cf2b09911ec40
Sharon Zhou <sharonzhou12@gmail.com>
2017-10-08 23:49:21 -0700

    db

--- single/app/__init__.py
+++ single/app/__init__.py
@@ -5,7 +5,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE"]
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 db = SQLAlchemy(app)
 
 app.config.from_object(__name__)
