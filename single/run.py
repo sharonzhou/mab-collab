684ba60567e8e177dd5ad2bcc7126a07b561bc31
Sharon Zhou <sharonzhou12@gmail.com>
2017-10-09 01:07:02 -0700

    web process

--- single/run.py
+++ single/run.py
@@ -1,8 +1,3 @@
 from app import app
-import os
 
-# app.run(debug=True)
-if __name__ == '__main__':
-	app.debug = True
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+app.run(debug=True)
