34f1e9f39b7853c58ce827bd0b383a9fae8af9f2
Sharon Zhou <sharonzhou12@gmail.com>
2018-01-12 12:17:01 -0800

    greedy discount factor

--- new_collab_model/analysis.py
+++ new_collab_model/analysis.py
@@ -6,13 +6,13 @@ def analyze(stats):
     def value(row):
         ret = row['reward']*1. # Next step's reward, 1 could be a parameter we'd need to fit/change
         discount = 1.
+        # discount = 0 # greedy
         player = row['player']
         for i in range(int(row['move'])+1, 15):
             discount *= .9 # Discount factor, need to fit/change
             player = 1-player
             ret += discount*row['exploit{}'.format(int(player))]*(1. if player==row['player'] else 1.) #weights of partner and me can be fit/changed
         return ret
-    print(stats[stats['chosen_arm']!=stats['arm']])
     stats = stats.assign(value=stats.apply(value, axis=1))
     best = stats.groupby(['room', 'game', 'move'])['value'].max().rename('best')
     stats = stats.join(best, on=['room', 'game', 'move'])
@@ -20,12 +20,10 @@ def analyze(stats):
     winners = stats.groupby(['room', 'game', 'move'])['winner'].sum().rename('others')
     stats = stats.join(winners, on=['room', 'game', 'move'])
     stats = stats.assign(agreement=stats['winner']/stats['others'])
-    # print(stats)
     stats = stats[stats['chosen_arm']==stats['arm']]
-    # print(stats)
-    # print(stats.groupby(['room', 'game', 'condition'])['agreement'].mean())
-    # print(stats.groupby(['room', 'condition'])['agreement'].mean())
-    # print(stats.groupby(['condition'])['agreement'].mean())
+    print(stats.groupby(['room', 'game', 'condition'])['agreement'].mean())
+    print(stats.groupby(['room', 'condition'])['agreement'].mean())
+    print(stats.groupby(['condition'])['agreement'].mean())
 
 if __name__=='__main__':
     print("starting", time.time())
@@ -40,6 +38,6 @@ if __name__=='__main__':
     results = pd.read_csv('intermediate_stats.csv')
     analyze(results)
 
-    # print(len(rooms), rooms)
-    # print(condition_counts)
+    print(len(rooms), rooms)
+    print(condition_counts)
 
